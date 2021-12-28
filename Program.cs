1e95aba545cf25b6a3ae513fde6b94d7c97cf9f6
Rishi Verma <riverma@microsoft.com>
2021-12-29 02:39:25 +0530

    draft

--- Program.cs
+++ Program.cs
@@ -7,6 +7,18 @@ namespace Algoexpert
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            int[] array = { -1, -2, 3, 4, -5, 6,-9 };
+            int[] answer = new int[array.Length];
+            int largest = answer[0];
+            int smallest = answer[0];
+            for (int i = 0; i < array.Length; i++)
+            {
+                answer[i] = Math.Abs(array[i]);
+            }
+            foreach (int item in answer)
+            {
+                
+            }
         }
     }
 }
