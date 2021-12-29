27ad5fa569a206fe7d3d59ceb8dbbd78e1e1b524
Rishi Verma <riverma@microsoft.com>
2021-12-29 17:54:11 +0530

    final

--- Program.cs
+++ Program.cs
@@ -9,16 +9,17 @@ namespace Algoexpert
             Console.WriteLine("Hello World!");
             int[] array = { -1, -2, 3, 4, -5, 6,-9 };
             int[] answer = new int[array.Length];
-            int largest = answer[0];
-            int smallest = answer[0];
+            int[] sortedSquares = new int[array.Length];
             for (int i = 0; i < array.Length; i++)
             {
                 answer[i] = Math.Abs(array[i]);
             }
-            foreach (int item in answer)
+            Array.Sort(answer);
+            for (int i = 0; i < array.Length; i++)
             {
-                
+                sortedSquares[i] = answer[i] * answer[i];
             }
+
         }
     }
 }
