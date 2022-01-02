8c5dab2f3d2a31824425d733de900d8bbf585d41
Rishi Verma <riverma@microsoft.com>
2022-01-02 15:02:35 +0530

    final array question

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Algoexpert.Questions;
+using System;
 
 namespace Algoexpert
 {
@@ -7,18 +8,8 @@ namespace Algoexpert
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            int[] array = { -1, -2, 3, 4, -5, 6,-9 };
-            int[] answer = new int[array.Length];
-            int[] sortedSquares = new int[array.Length];
-            for (int i = 0; i < array.Length; i++)
-            {
-                answer[i] = Math.Abs(array[i]);
-            }
-            Array.Sort(answer);
-            for (int i = 0; i < array.Length; i++)
-            {
-                sortedSquares[i] = answer[i] * answer[i];
-            }
+            SortedSquaredArray obj = new SortedSquaredArray();
+            obj.SortedSquaredArrayCode();
 
         }
     }
