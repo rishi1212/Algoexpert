be5dafdd0dcbbbffd6715356af0f126c07646675
Rishi Verma <riverma@microsoft.com>
2023-12-27 01:28:48 +0530

    changes

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using Algoexpert.Questions;
+using Algoexpert.Questions.Leetcode;
 using System;
+using System.Linq;
 
 namespace Algoexpert
 {
@@ -7,9 +9,12 @@ namespace Algoexpert
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            SortedSquaredArray obj = new SortedSquaredArray();
-            obj.SortedSquaredArrayCode();
+            EasyTagQuestions obj = new EasyTagQuestions();
+            int[] answer = obj.TwoSum([2,7,11,15],9);
+            foreach (int i in answer)
+            {
+                Console.WriteLine(i);
+            }
 
         }
     }
---
+++ Questions/Leetcode/EasyTagQuestions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoexpert.Questions.Leetcode
+{
+    internal class EasyTagQuestions
+    {
+
+        // question 1
+        public int[] TwoSum(int[] nums, int target)
+        {
+
+
+            int[] result = { 0, 0 };
+            Hashtable hashtable = new Hashtable();
+            hashtable.Add(0, nums);
+
+            return result;
+
+        }
+    }
+}
